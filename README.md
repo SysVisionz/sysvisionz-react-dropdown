e105cd5590f7d5e65ec2d33c76bced5a06ff9612
SysVisionz <colin@sysvisionz.com>
2019-10-17 13:49:15 -0600

    complete refactor

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This module allows you to utilize a simple, highly customizable dropdown menu th
 To install, in terminal type
 
 ```
-	npm i --save-dev sysvisionz-react-dropdown
+	npm i --save sysvisionz-react-dropdown
 ```
 
 then, in your react project,
@@ -27,42 +27,42 @@ and finally, implement it by including that component within your code:
 
 ### Necessary Variables
 
-#### label
+#### button
 **accepted variables:** any string or JSX element  
 **behavior:** dictates the children of the activating button.  
 
-#### entries
+#### content
 
-**accepted variables:** array of strings, JSX elements, or objects with ```id``` and ```children``` properties.  
+**accepted variables:** array of strings or JSX elements  
 **behavior:** dictates the children of the entries in the dropdown menu.  If split into ```id``` and ```children```, the ```id``` property is applied as the id of the div containing this array element, and the ```children``` property dictates the children of that element.
 
 ### Optional Variables
 
-#### dropDirection
+#### drop
 **accepted variables:** 'up', 'down', 'left', 'right'  
-**behavior:** Defaults to 'down' variable if not specified. This dictates the direction that your dropdown menu drops out of the activating button. downLeft and upLeft are special variants; they render a normal dropDirection, but orient the menu's edge to the opposite of the basic version (to the right edge for downLeft and upLeft, and to the bottom edge for rightUp and leftUp) to the activating button instead of the left edge. Note that, due to their nature, they make the popDirection variable unnecessary by definition.
+**behavior:** Defaults to 'down' variable if not specified. This dictates the direction that your dropdown menu drops out of the activating button. downLeft and upLeft are special variants; they render a normal drop, but orient the menu's edge to the opposite of the basic version (to the right edge for downLeft and upLeft, and to the bottom edge for rightUp and leftUp) to the activating button instead of the left edge. Note that, due to their nature, they make the pop variable unnecessary by definition.
 
-#### popDirection
+#### pop
 **accepted variables:** 'up', 'down', 'left', 'right'  
-*cannot be opposite of dropDirection.*  
-**behavior:** Defaults to dropDirection variable if not specified. This dictates the direction that your dropdown menu pops outwards from the inital dropped out element.
+*cannot be opposite of drop.*  
+**behavior:** Defaults to drop variable if not specified. This dictates the direction that your dropdown menu pops outwards from the inital dropped out element.
 
 #### orientation
 **accepted variables:** 'top', 'bottom', 'left', 'right', 'center'  
-*top, bottom, left, and right can only be applied when **popDirection** prop is not used. center may always be used*  
-*note that top and bottom can only be used with dropDirection of left or right, and left or right with dropDirection up or down*   
-**behavior:** controls where the dropdown menu is oriented in relation to its supplied dropDirection. orientation center will make a menu dropping downwards appear centered beneath the activating button, where orientation left will make the right edge of the menu align with the right edge of the activating button.
+*top, bottom, left, and right can only be applied when **pop** prop is not used. center may always be used*  
+*note that top and bottom can only be used with drop of left or right, and left or right with drop up or down*   
+**behavior:** controls where the dropdown menu is oriented in relation to its supplied drop. orientation center will make a menu dropping downwards appear centered beneath the activating button, where orientation left will make the right edge of the menu align with the right edge of the activating button.
 
 #### controlled
 **accepted variables:** boolean  
-**behavior:** dictates whether the menu is opened and closed by an outside variable (which is supplied through the isOpen variable)
+**behavior:** dictates whether the menu is opened and closed by an outside variable (which is supplied through the **open** variable)
 
 #### keepOpen
 **accepted variables:** boolean  
 **behavior:** Defaults to false, unless **controlled** is true, then it defaults to true. Dictates if dropdown menu will stay open when clicking outside it or on the entries in the menu. Menu can still be closed by clicking on the activating button
 **note:** if you do set this to false while controlled is true, it is highly recommended that you use the onToggle function to reset your isOpen variable accordingly!
 
-#### onSelect
+#### onChange
 **accepted variables:** function  
 **behavior:** Function performed when a dropdown menu item is clicked using the entry clicked.
 
@@ -78,57 +78,17 @@ and finally, implement it by including that component within your code:
 **accepted variables:** function  
 **behavior:** function performed when menu is closed.
 
-#### delay
-**accepted variables:** number  
-**behavior:** delay before closing the menu.  
-
-#### clickableInDelay
-**accepted variables:** boolean  
-**behavior:** if menu is clickable during the delay when closing.
-
-#### id
-**accepted variables:** string  
-**behavior:** dictates the id for the overall element.
-
 #### className
 **accepted variables:** string  
 **behavior:** dictates the className for the overall element. Note that this is, by design, followed by the "open" or "closed" class, depending on the state of the menu.
-
-#### style
-**accepted variables:** string  
-**behavior:** dictates the style for the overall element.
-
-#### buttonId
-**accepted variables:** string  
-**behavior:** dictates the id property for the activating button.
 
 #### buttonClass
 **accepted variables:** string  
 **behavior:** dictates the className for the activating button.
 
-#### buttonStyle
+#### id
 **accepted variables:** string  
-**behavior:** dictates the style for the activating button.
-
-#### menuId
-**accepted variables:** string  
-**behavior:** dictates the id for the dropdown menu element.
-
-#### menuClass
-**accepted variables:** string  
-**behavior:** dictates the className for the dropdown menu element.
-
-#### menuStyle
-**accepted variables:** string  
-**behavior:** dictates the style for the dropdown menu element.
-
-#### listClass
-**accepted variables:** string  
-**behavior:** dictates the className for all individual items in the dropdown menu.
-
-#### listStyle
-**accepted variables:** JSX styling  
-**behavior:** dictates styling for all individual items in the dropdown menu.
+**behavior:** dictates the id for the dropdown containing div
 
 
 ### Prerequisites
@@ -147,7 +107,7 @@ implemented ability to use any JSX element as activating button
 implemented keepOpen prop.  
 1.2 -  
 resolved location issues with package.json  
-reversed dropDirection and popDirection for clarity.  
+reversed drop and pop for clarity.  
 cleaner implementation of several code sections.  
 1.3 -  
 allowed for multiple dropdowns to be generated.  
@@ -163,7 +123,7 @@ added delay and clickableInDelay properties.
 1.8 -  
 significant refactor for reliability, removal of retrieved redundant boolean on onToggle.  
 1.9 -  
-implementation of leftUp, rightUp, upLeft and downLeft dropDirection options.
+implementation of leftUp, rightUp, upLeft and downLeft drop options.
 
 2.0 -  
 Complete refactor and bugfixes  
@@ -175,3 +135,10 @@ addition of controlled option.
 Huge bugfix  
 introduction of open and closed automatic class name.  
 now properly allows for animations  
+
+3.0 -  
+Complete refactor, removal of many extraneous variables, actual complete bugfixes.  
+3.1 -  
+Controlled variable reintroduced properly.  
+3.2 -  
+Bugfix on controlled variable and keepOpen
--- src/index.js
+++ src/index.js
@@ -1,327 +1,176 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import "./scss/dropdown.scss";
 //usable props: label, entries, dropDirection, popDirection, orientation, isOpen, controlled, keepOpen
 //onToggle, onOpen, onClose
-//id, className, style, buttonId, buttonClass, buttonStyle, menuId, menuClass, menuStyle.
+//id, className, style, 
 
-const listStyle = (dropDirection, popDirection, orientation, horizontal, offset) => {
-	let style = {
-		position: 'absolute', 
-		display: 'flex', 
-		right: undefined, 
-		left: undefined, 
-		top: undefined, 
-		bottom: undefined
-	}
-	if ((horizontal.pop == null && horizontal.drop) || horizontal.pop){
-			style.flexDirection = 'row';
-	}
-	else{
-			style.flexDirection = 'column';
-	}
-	switch(dropDirection){
-		case 'up':
-			style.bottom = '100%'
-			switch(popDirection){
-				case 'left':
-					style.right=0;
-					break;
-				case 'right':
-					style.left=0;
-					break;
-			}
-			break;
-		case 'left':
-			style.right='100%';
-			switch(popDirection){
-				case 'up':
-					style.bottom = 0;
-					break;
-				case 'down':
-				default:
-					style.top=0
-					break;
-			}
-			break;
-		case 'right':
-			style.left='100%'
-			switch(popDirection){
-				case 'up':
-					style.bottom=0;
-					break;
-				case 'down':
-				default:
-					style.top=0;
-					break;
-			}
-			break;
-		case 'down':
-		default:
-			switch(popDirection){
-				case 'left':
-					style.right= 0;
-					break;
-				case 'right':
-					style.left= 0;
-					break;
-			}
-			break;
-	}
-	if (!popDirection) {
-		switch (orientation) {
-			case 'top':
-				if ((horizontal.pop == null || horizontal.pop == horizontal.drop) && horizontal.drop) {
-					style.bottom=0;
-					style.top=undefined;
-				}
-				break;
-			case 'bottom':
-				if ((horizontal.pop == null || horizontal.pop == horizontal.drop) && horizontal.drop) {
-					style.top=0;
-					style.bottom=undefined
-				}
-				break;
-			case 'right':
-				if ((horizontal.pop == null || horizontal.pop == horizontal.drop) && !horizontal.drop) {
-					style.left=0;
-					style.right=undefined;
-				}
-				break;
-			case 'left':
-				if ((horizontal.pop == null || horizontal.pop == horizontal.drop) && !horizontal.drop) {
-					style.right=0;
-					style.left=undefined;
-				}
-				break;
-			default:
-				break;
+const filterJoin = (arr, joinVal = ' ') => {
+	for (const i in arr){
+		if(arr[i] && typeof arr[i] === 'object'){
+			arr[i] = arr[i][1] ? arr[i][0] : null;
 		}
 	}
-	if (offset.top) {
-		style.top = offset.top;
-		style.bottom = undefined;
-	}
-	if (offset.left) {
-		style.left = offset.left;
-		style.right = undefined;
-	}
-	return style;
+	return arr.filter(a => a).join(joinVal)
 }
 
 export default class Dropdown extends Component {
 
-	constructor(props) {
-		super(props);
-		this.identifier = Math.floor(Math.random()*1000000);
-		this.state ={
-			isOpen: props.isOpen,
-			openClass: props.isOpen,
-			listClickable: true,
-			offset: {top: undefined, left: undefined},
-			// if controlled is true, keepOpen defaults to true, unless otherwise defined.
-			keepOpen: props.controlled && props.keepOpen == undefined ? props.controlled : props.keepOpen
+	constructor(props){
+		super();
+		this.openButton = React.createRef();
+		this.contentDiv = React.createRef();
+		this.containerDiv = React.createRef();
+		this.state = {open: props.isOpen || false}
+		this.id=Math.round(Math.random()*1000);
+	}
+	
+	popStyling = () => {
+		const {open} = this.state;
+		const {transitionTime = 400, fadeIn, slideIn, drop, pop} = this.props;
+		const horizontal = {
+			drop: drop === 'left' || drop === 'right',
+			pop: pop === 'left' || pop ==='right'
 		}
+		let style = {
+			width: (open || !horizontal.pop) && this.contentDiv.current ? this.contentDiv.current.scrollWidth : 0,
+			height: (open || horizontal.pop) && this.contentDiv.current ? this.contentDiv.current.scrollHeight: 0,
+		}
+		let transition = [];
+		if (fadeIn){
+			transition.push('opacity ' + transitionTime + 'ms ease');
+		}
+		if (slideIn){
+			transition.push('width ' + (horizontal.pop ? transitionTime + 'ms' : '0ms') + ' ease');
+			transition.push('height ' + (!horizontal.pop ? transitionTime + 'ms' : '0ms') + ' ease');
+		}
+		style.transition=transition.join(', ')
+		return style;
 	}
 
-	componentWillUnmount() {
-		// clean up onclick listener if it exists.
-		document.removeEventListener('click', this.onClickClose);
-	}
-
-	selectedItem = (selection) => {
-		// run onSelect function if it exists, and close it if keepOpen prop isn't true.
-		if (this.props.onSelect) { this.props.onSelect(selection) }
-		if (!this.state.keepOpen) { this.setState({isOpen: false}) }
-	}
-
-	componentWillReceiveProps(nextProps){
-		// if this is controlled, run the menuToggle function if the isOpen prop has been toggled.
-		if (this.props.controlled && this.props.isOpen != nextProps.isOpen){
-			this.menuToggle(!nextProps.isOpen);
+	toggleMenu = open => {
+		const {onToggle, onOpen, onClose} = this.props;
+		if (onToggle){
+			onToggle(open);
 		}
-		// if controlled status or keepOpen status change due to adjustments, change keepOpen state accordingly.
-		if (nextProps.keepOpen != this.state.keepOpen || nextProps.controlled != this.props.controlled){
-			this.setState({keepOpen: nextProps.controlled && nextProps.keepOpen == undefined ? nextProps.controlled : nextProps.keepOpen});
-		}
-	}
-
-	componentDidMount() {
-		if (this.state.isOpen &&  this.props.orientation == 'center') {
-			this.centerMenu()
-		}
-		if (this.props.isOpen && !this.state.keepOpen){
-			document.addEventListener('click', this.onClickClose);
-		}
-	}
-
-	componentDidUpdate(prevProps, prevState) {
-		// Check if update is because list opened or closed.
-		if (prevState.isOpen != this.state.isOpen){
-			// Checks if component has onToggle, onOpen, or OnClose functions and runs them accordingly.
-			if ( this.props.onToggle ) { this.props.onToggle(this.state.isOpen) }
-			if ( this.props.onOpen && this.state.isOpen ) { this.props.onOpen() }
-			if ( this.props.onClose && !this.state.isOpen ) { this.props.onClose() }
-		}
-		if ( (this.state.isOpen && this.props.orientation == 'center') && (this.props != prevProps || !prevState.isOpen)){
-			this.centerMenu();
-		}
-		else if (prevProps.orientation == 'center' && this.props.orientation != 'center'){
-			this.setState({offset: {top: undefined, left: undefined}})
-		}
-	}
-
-	centerMenu = () => {
-		const topOffset = (document.getElementById(this.buttonId).offsetHeight - document.getElementById(this.menuId).offsetHeight)/2;
-		const leftOffset = (document.getElementById(this.buttonId).offsetWidth - document.getElementById(this.menuId).offsetWidth)/2;
-		if (!this.horizontal.drop){
-			this.setState({offset: {top: undefined, left: leftOffset}});
-		}
-		else {
-			this.setState({offset: {top: topOffset, left: undefined}})
-		}
-	}
-
-	openMenu = () => {
-		// open the menu and make it clickable again, then add the event listener for onClickClose
-		// if keepOpen prop isn't true.
-		this.setState({isOpen: true, listClickable: true});
-		this.mayAnimate = setTimeout (() => this.setState({openClass: true}), 50);
-		if (!this.state.keepOpen){
-			document.addEventListener('click', this.onClickClose);
-		}
-	}
-
-	onClickClose = evt => {
-		// test if you clicked within the menu, close it if you didn't.
-		if (!evt.target.closest('.'.concat(this.menuId))){
-			this.setState({openClass: false})
-			this.delayer = setTimeout(this.closeMenu, this.props.delay)
-			document.removeEventListener('click', this.onClickClose);
-		}
-	}
-
-	menuToggle = open => {
-		if (this.delayer) {
-			clearTimeout(this.delayer);
-		}
-		if (!this.props.clickableInDelay) {this.setState({listClickable: false})};
-		//toggle menu to open or close depending on whether the menu is open at the moment, using the delay if the menu us closing.
 		if (open){
-			this.setState({openClass: false});
-			this.delayer = setTimeout(this.closeMenu, this.props.delay);
-		}
-		else {
-			this.openMenu();
-		}
-	}
-
-	closeMenu = () => {
-		this.setState({isOpen: false, listClickable: true});
-		document.removeEventListener('click', this.onClickClose);
-	}
-
-	buttonClick = open => {
-		//triggered by the activating button, if the menu is not opened or closed by another trigger, open it.
-		if (!this.props.controlled) {
-			this.menuToggle(open);
-		}
-	}
-
-	renderMenu = () => {
-		return this.props.entries.map((entry, index) => {
-			//if entry is constructed with an id variable, use that
-			if (entry.id || entry.className) {
-				return (
-					<div 
-						key={'svzDropButton' + this.identifier + index} 
-						id={entry.id} 
-						className={entry.className ? entry.className : "listEntry"} 
-						onClick ={() => {this.selectedItem(entry.children)}}
-						style={this.props.listItemStyle}
-					>
-						{entry.children}
-					</div>
-				);
+			document.addEventListener('click', this.clickOutside)
+			clearTimeout(this.openDelay)
+			if (onOpen){
+				onOpen();
 			}
-			else {
-				//if entry is constructed without an id variable or class, assume use as a basic component
-				return (
-				<div 
-					key={'svzDropButton' + this.identifier + index} 
-					className={(this.props.listClass || "listEntry")} 
-					onClick={() => {this.selectedItem(entry)}}
-					style = {this.props.listStyle}
-				>
-					{entry}
-				</div>);
+		}
+		if (!open){
+			document.removeEventListener('click', this.clickOutside);
+			if(onClose){
+				onClose();
 			}
-		});
+		}
+		this.setState({open})
 	}
 
-	render() {
-		const {
-			state, 
-			props, 
-			renderMenu, 
-			menuToggle, 
-			buttonClick,
-			identifier,
-		} = this;
-		const {
-			dropDirection, 
-			popDirection, 
-			orientation, 
-			label,
-			id, 
-			menuId,
-			className, 
-			menuClass, 
-			buttonClass,
-			style, 
-			menuStyle,
-			buttonStyle,
-			controlled
-		} = props;
-		const {
-			listClickable, 
-			isOpen,
-			offset,
-			openClass
-		} = state;
-		const drop = dropDirection == 'left' || dropDirection == 'right';
-		const pop = popDirection ? popDirection == 'left' || popDirection =='right' : null;
-		this.horizontal = {drop, pop};
-		const popStyle = listStyle(dropDirection, popDirection, orientation, this.horizontal, offset);
-		this.buttonId = (props.buttonId || 'svzDropButton' + identifier);
-		this.menuId = (menuId || 'svzMenu' + identifier);
-		const cName = className ? className : '';
-		return (
-			<div 
-				style = {{position: 'relative', display: 'inline-block', ...style}} 
-				className={openClass ? cName+" open" : cName + " closed"} 
-				id={id}
+	componentDidUpdate = (prevProps, prevState) => {
+		if (this.props.controlled){
+			const {open = this.state.open} = this.props;
+			if (this.state.open !== open){
+				this.toggleMenu(open);
+			}
+		}
+	}
+
+	componentDidMount(){
+		const {toggleMenu} = this;
+		toggleMenu(this.props.open);
+	}
+
+	clickOutside = () => {
+		if (!this.props.keepOpen){
+			setTimeout(() => {
+				if (this.clickedMenu){
+					this.clickedMenu = false;
+				}
+				else {
+					this.toggleMenu(false);
+				}
+			}, 5)
+		}
+	}
+
+	onChange = retval => {
+		const {onChange, keepOpen} = this.props;
+		if (!keepOpen){
+			this.toggleMenu(false);
+		}
+		return onChange ? onChange(retval) : null;
+	}
+
+	buildButton = () => {
+		const {props, state, toggleMenu} = this;
+		const {button} = props;
+		const {open} = state;
+		return (<div ref={this.openButton} className="svz-dropdown-button" onClick={() => this.props.controlled ? null : toggleMenu(!open) }>{button}</div>)
+	}
+
+	buildMenu = () => {
+		const {onChange} = this;
+		const {content, reverse} = this.props;
+		const elements = [];
+		for (const i in content){
+			const elem = <div 
+				key= {"svz-dropdown-list-elem-"+this.id +i} 
+				onClick={() => onChange(React.isValidElement(content[i]) ? i : content[i])}
+				className={'svz-dropdown-list-elem'}
 			>
-				<div 
-					id={this.buttonId}
-					className={buttonClass}
-					style = {{cursor: controlled ? 'pointer' : 'auto', ...buttonStyle}} 
-					onClick={() => buttonClick(isOpen)}
-				>
-					{label}
-				</div>
-				<div 
-					style = {{pointerEvents: listClickable ? 'auto' : 'none'}} 
-					hidden={!isOpen}
-				>
+				{content[i]}
+			</div>
+			reverse ? elements.splice(0, 0, elem) : elements.push(elem);
+		}
+		return elements;
+	}
+
+	menuClass = () => {
+		const {open} = this.state;
+		const {menuClass, drop = 'down', orientation} = this.props;
+		const {pop = drop} = this.props;
+		return filterJoin(["svz-dropdown-menu", 'drop-' + drop, 'pop-' + pop, ['orient-' + orientation, orientation], ['active', open], menuClass])
+	}
+
+	render(){
+		const {buildButton, buildMenu, popStyling} = this;
+		const {id, className, keepOpen} = this.props;
+		const {open} = this.state;
+		const menuStyle = popStyling();
+		return (
+			<div id={id} className={filterJoin(["svz-dropdown-container", className, ['active', open]])}>
+				<div className="svz-dropdown-sub-container" >
+					{buildButton()}
 					<div 
-						className={menuClass} 
-						id={this.menuId} 
-						style={{...popStyle, ...menuStyle}}
+						ref={this.containerDiv} 
+						style={menuStyle} 
+						className={this.menuClass()}
+						onClick = {() => this.clickedMenu = keepOpen ? true : false} 
 					>
-						{renderMenu()}
+						<div 
+							className={'menu-content'}
+							ref={this.contentDiv} 
+						>
+							{buildMenu()}
+						</div>
 					</div>
 				</div>
 			</div>
-		);
+		)
 	}
+}
+
+Dropdown.propTypes={
+	onChange: PropTypes.func,
+	options: PropTypes.array,
+	orientation: PropTypes.string,
+	onClose: PropTypes.func,
+	onOpen: PropTypes.func,
+	pop: PropTypes.string,
+	drop: PropTypes.string,
+	fadeIn: PropTypes.bool,
+	slideIn: PropTypes.bool,
 }
